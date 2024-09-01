bf448d0d5557b078c48cc9716d696e946b012e46
Battletoadster <72815818+Battletoadster@users.noreply.github.com>
2024-09-01 21:12:26 +0200

    Add round and full game logic

--- rps.js
+++ rps.js
@@ -1,12 +1,54 @@
+let playerScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    if (choice === 0){
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0){
         return "rock";
-    } else if (choice === 1){
+    } else if (computerChoice === 1){
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice(){
+    let playerChoice = prompt("Choose rock, paper or scissors");
+    return playerChoice;
+}
+
+function playRound(humanChoice, computerChoice){
+    let lowerHumanChoice = humanChoice.toLowerCase();
+    let lowerComputerChoice = computerChoice.toLowerCase();
+
+    if (lowerComputerChoice === lowerHumanChoice){
+        console.log(`Tie! You both chose ${lowerComputerChoice}`)
+    } else if ((lowerComputerChoice === "rock" && lowerHumanChoice === "scissors") 
+                || (lowerComputerChoice === "paper" && lowerHumanChoice === "rock") 
+                || (lowerComputerChoice === "scissors" && lowerHumanChoice === "paper")){
+                    console.log(`You lose! ${lowerComputerChoice.charAt(0).toUpperCase() + lowerComputerChoice.slice(1)} beats ${lowerHumanChoice}!`);
+                    computerScore++;
+    } else {
+        console.log(`You win! ${lowerHumanChoice.charAt(0).toUpperCase() + lowerHumanChoice.slice(1)} beats ${lowerComputerChoice}!`);
+        playerScore++;
+    }
+}
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        const playerChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        playRound(playerChoice, computerChoice);
+    }
+}
+
+playGame();
+
+if (playerScore === computerScore){
+    console.log("It's a tie!");
+} else if (playerScore > computerScore){
+    console.log("You win!")
+} else {
+    console.log('You lose!')
+}
+console.log(`Player: ${playerScore}\nComputer: ${computerScore}`);
