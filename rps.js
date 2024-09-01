0dd907bbbedf4109fdd3262115842c997bdaab0d
Battletoadster <72815818+Battletoadster@users.noreply.github.com>
2024-09-01 21:16:57 +0200

    Format javascript code

--- rps.js
+++ rps.js
@@ -1,41 +1,41 @@
 let playerScore = 0;
 let computerScore = 0;
 
-function getComputerChoice(){
+function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3);
-    if (computerChoice === 0){
+    if (computerChoice === 0) {
         return "rock";
-    } else if (computerChoice === 1){
+    } else if (computerChoice === 1) {
         return "paper";
     } else {
         return "scissors";
     }
 }
 
-function getHumanChoice(){
+function getHumanChoice() {
     let playerChoice = prompt("Choose rock, paper or scissors");
     return playerChoice;
 }
 
-function playRound(humanChoice, computerChoice){
+function playRound(humanChoice, computerChoice) {
     let lowerHumanChoice = humanChoice.toLowerCase();
     let lowerComputerChoice = computerChoice.toLowerCase();
 
-    if (lowerComputerChoice === lowerHumanChoice){
+    if (lowerComputerChoice === lowerHumanChoice) {
         console.log(`Tie! You both chose ${lowerComputerChoice}`)
-    } else if ((lowerComputerChoice === "rock" && lowerHumanChoice === "scissors") 
-                || (lowerComputerChoice === "paper" && lowerHumanChoice === "rock") 
-                || (lowerComputerChoice === "scissors" && lowerHumanChoice === "paper")){
-                    console.log(`You lose! ${lowerComputerChoice.charAt(0).toUpperCase() + lowerComputerChoice.slice(1)} beats ${lowerHumanChoice}!`);
-                    computerScore++;
+    } else if ((lowerComputerChoice === "rock" && lowerHumanChoice === "scissors")
+        || (lowerComputerChoice === "paper" && lowerHumanChoice === "rock")
+        || (lowerComputerChoice === "scissors" && lowerHumanChoice === "paper")) {
+        console.log(`You lose! ${lowerComputerChoice.charAt(0).toUpperCase() + lowerComputerChoice.slice(1)} beats ${lowerHumanChoice}!`);
+        computerScore++;
     } else {
         console.log(`You win! ${lowerHumanChoice.charAt(0).toUpperCase() + lowerHumanChoice.slice(1)} beats ${lowerComputerChoice}!`);
         playerScore++;
     }
 }
 
-function playGame(){
-    for(let i = 0; i < 5; i++){
+function playGame() {
+    for (let i = 0; i < 5; i++) {
         const playerChoice = getHumanChoice();
         const computerChoice = getComputerChoice();
         playRound(playerChoice, computerChoice);
@@ -44,9 +44,9 @@ function playGame(){
 
 playGame();
 
-if (playerScore === computerScore){
+if (playerScore === computerScore) {
     console.log("It's a tie!");
-} else if (playerScore > computerScore){
+} else if (playerScore > computerScore) {
     console.log("You win!")
 } else {
     console.log('You lose!')
